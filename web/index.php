4045c18d030539bd97ba5882e1db2dd64a5e18e2
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-04-23 23:23:29 +0430

    include paths.php

--- web/index.php
+++ web/index.php
@@ -3,5 +3,6 @@
 use Rad\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__) . '/src/Config/paths.php';
 
-Application::getInstance()->run();
+Application::getInstance()->runWeb();
