bcae7fb3d32cd70fbf9845318b8521f7f10c05d9
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-05-13 23:25:05 +0430

    Update provision script

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+echo 'DROP DATABASE IF EXISTS radphp_db;' | sudo -u postgres psql
+echo "DROP ROLE IF EXISTS radphp_user;" | sudo -u postgres psql
+echo "CREATE USER radphp_user WITH PASSWORD 'rad123';" | sudo -u postgres psql
+echo "CREATE DATABASE radphp_db;" | sudo -u postgres psql
+echo "GRANT ALL PRIVILEGES ON DATABASE radphp_db to radphp_user;" | sudo -u postgres psql
