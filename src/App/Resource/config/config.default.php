0e37de72e8dea09fe16de282dfde8f419dc72b16
Omid Mottaghi <omidmr@gmail.com>
2015-07-14 19:32:56 +0430

    add database connection info to config

--- src/App/Resource/config/config.default.php
+++ src/App/Resource/config/config.default.php
@@ -6,9 +6,26 @@ return [
         'possible' => ['en'],
         'default' => 'en'
     ],
+    'cake_orm' => [
+        'datasources' => [
+            'default' => [
+                'className' => 'Cake\Database\Connection',
+                'driver' => 'Cake\Database\Driver\Postgres',
+                'persistent' => false,
+                'host' => 'localhost',
+                'username' => 'radphp_user',
+                'password' => 'rad123',
+                'database' => 'radphp_db',
+                'encoding' => 'utf8',
+                'timezone' => 'UTC',
+                'cacheMetadata' => true
+            ]
+        ]
+    ],
     'bundles' => [
         'App' => [],
         'Twig' => [],
         'Pages' => [],
+        'CakeOrm' => [],
     ]
 ];
