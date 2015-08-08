0f93ebef10ddd9fc36e79a3d0633f8d5e401ecdf
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-08-08 20:14:22 +0430

    Remove Response

--- web/index.php
+++ web/index.php
@@ -5,4 +5,4 @@ use Rad\Network\Http\Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-\Application::getInstance()->runWeb(new Request(), new Response());
+\Application::getInstance()->handleWeb(new Request());
