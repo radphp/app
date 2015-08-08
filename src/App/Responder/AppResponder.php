ea1fbaacf77c28a25b60d2f7802096bd29a6faec
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-08-08 20:12:56 +0430

    Added "render" method

--- src/App/Responder/AppResponder.php
+++ src/App/Responder/AppResponder.php
@@ -3,6 +3,8 @@
 namespace App\Responder;
 
 use Rad\Core\Responder;
+use Rad\Network\Http\Response;
+use SimpleXMLElement;
 
 /**
  * App Responder
@@ -11,5 +13,35 @@ use Rad\Core\Responder;
  */
 class AppResponder extends Responder
 {
+    /**
+     * Render template
+     *
+     * @param string $template
+     * @param array  $params
+     *
+     * @return Response
+     * @throws \Rad\DependencyInjection\Exception\ServiceNotFoundException
+     */
+    protected function render($template, $params)
+    {
+        switch ($this->getRequest()->getMediaType()) {
+            case 'application/json':
+                $content = json_encode($params);
+                break;
 
+            case 'application/xml':
+                $xml = new SimpleXMLElement('<root/>');
+                array_walk_recursive($params, [$xml, 'addChild']);
+                $content = $xml->asXML();
+                break;
+
+            default:
+                /** @var \Twig_Environment $twig */
+                $twig = $this->getContainer()->get('twig');
+                $content = $twig->render($template, $params);
+        }
+
+
+        return new Response($content);
+    }
 }
