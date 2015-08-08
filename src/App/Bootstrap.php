f2348f56d19265e2de429a4c23a8e0e3be5ae8f2
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-08-08 20:11:29 +0430

    Remove Middlewares

--- src/App/Bootstrap.php
+++ src/App/Bootstrap.php
@@ -3,9 +3,6 @@
 namespace App;
 
 use Rad\Core\Bundle;
-use Rad\Routing\Middleware\DispatcherMiddleware;
-use Rad\Routing\Middleware\RouterMiddleware;
-use Rad\Routing\MiddlewareCollection;
 
 /**
  * App Bootstrap
@@ -14,16 +11,5 @@ use Rad\Routing\MiddlewareCollection;
  */
 class Bootstrap extends Bundle
 {
-    /**
-     * Startup bundle
-     */
-    public function startup()
-    {
-        parent::startup();
 
-        if ('cli' !== PHP_SAPI) {
-            MiddlewareCollection::getInstance()->add(new RouterMiddleware());
-            MiddlewareCollection::getInstance()->add(new DispatcherMiddleware());
-        }
-    }
 }
